18e174c08f8f90a63f8864c4c63d880b90f6d697
amelialinder <amelia.linder@marquette.edu>
2025-02-04 22:42:42 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -31,5 +31,5 @@ http.createServer(function (req, res) {
     });
 }).listen(8081);
 // Console will print the message
-console.log('Server running at http://
+console.log('Server running at http://127.0.0.1:8081/');
 
