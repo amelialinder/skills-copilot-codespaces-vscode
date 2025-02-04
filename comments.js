79f8ec4bf39113f40093cb868ea2eb76ddbc1e03
amelialinder <amelia.linder@marquette.edu>
2025-02-04 22:30:27 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+//create web server 
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//use cors to allow cross origin resource sharing
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
